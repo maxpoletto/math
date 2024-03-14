3555b9e1b2a93f14affae3de2fc22acbf95f6097
Max Poletto <maxp@google.com>
2024-03-14 10:43:18 +0100

    many changes: code restructuring, better-defined notion of logical viewport, properly responsive panning, faster rendering loop (fewer mul/div)

--- main.ts
+++ main.ts
@@ -1,53 +1,86 @@
-const lCenterX = document.getElementById('centerX') as HTMLSpanElement;
-const lCenterY = document.getElementById('centerY') as HTMLSpanElement;
-const lEventX = document.getElementById('eventX') as HTMLSpanElement;
-const lEventY = document.getElementById('eventY') as HTMLSpanElement;
-const lX0 = document.getElementById('x0') as HTMLSpanElement;
-const lY0 = document.getElementById('y0') as HTMLSpanElement;
-const lX1 = document.getElementById('x1') as HTMLSpanElement;
-const lY1 = document.getElementById('y1') as HTMLSpanElement;
-
+const lViewport = document.getElementById('viewport') as HTMLSpanElement;
+const lEvent = document.getElementById('event') as HTMLSpanElement;
 const canvas = document.getElementById('mandelbrotCanvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-//cliWidth = canvas.clientWidth;
-//cliHeight = canvas.clientHeight;'
+class Viewport {
+    public width: number;
+    public height: number;
+    public cx: number;
+    public cy: number;
 
-canvas.width = 800;
-canvas.height = 800;
-let canvasWidth = canvas.width;
-let canvasHeight = canvas.height;
+    constructor(w: number, h: number, cx: number, cy: number) {
+	this.width = w;
+	this.height = h;
+	this.cx = cx;
+	this.cy = cy;
+    }
+    public upperLeft() : [number, number] {
+	return [this.cx - this.width/2, this.cy - this.height/2];
+    }
+    public toString() : string {
+	return `(${this.cx}, ${this.cy}) ` +
+	    `[(${this.cx-this.width/2}, ${this.cy-this.height/2}),` +
+	    ` (${this.cx+this.width/2}, ${this.cy+this.height/2})]`;
+    }
+}
 
-let scale = 0.5;
-let centerX = -0.5, centerY = 0;
-let oldCenterX = centerX, oldCenterY = centerY;
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+const zoomFactor = 1.1;
+const defaultLogicalWidth = 2.5;
+const defaultLogicalCX = -0.75;
+const defaultLogicalCY = 0;
+let scale = canvas.clientWidth / defaultLogicalWidth;
+let logical = new Viewport(defaultLogicalWidth, canvas.clientHeight / scale,
+			   defaultLogicalCX, defaultLogicalCY);
+
+let oldCX = logical.cx, oldCY = logical.cy;
 let isPanning = false, isZooming = false;
 let startX = 0, startY = 0;
-const zoomFactor = 1.1;
 
 function updateURL() {
-    const hash = `#${centerX},${centerY},${scale}`;
+    const hash = `#${logical.cx},${logical.cy},${scale}`;
     history.replaceState(null, '', hash);
 }
+
+function updateLabels(event: MouseEvent) {
+    lViewport.textContent = logical.toString();
+    lEvent.textContent = `(${event.offsetX}, ${event.offsetY})`;
+}
+
+/*
+ * Window events (load, resize)
+ */
 
 window.addEventListener('load', () => {
     const hash = window.location.hash.substring(1); // Remove the '#'
     const parts = hash.split(',');
     if (parts.length === 3) {
-	centerX = parseFloat(parts[0]);
-	centerY = parseFloat(parts[1]);
+	logical.cx = parseFloat(parts[0]);
+	logical.cy = parseFloat(parts[1]);
 	scale = parseFloat(parts[2]);
     }
     drawMandelbrot();
 });
+
+// TODO: handle resize events
+
+/*
+ * Zoom
+ */
 
 let zoomTimeout : number;
 canvas.addEventListener('wheel', (event) => {
     event.preventDefault();
     if (event.deltaY < 0) { // zoom in
 	scale *= zoomFactor;
+	logical.width /= zoomFactor;
+	logical.height /= zoomFactor;
     } else {
 	scale /= zoomFactor;
+	logical.width *= zoomFactor;
+	logical.height *= zoomFactor;
     }    
     updateURL();
     if (!isZooming) {
@@ -64,23 +97,16 @@ canvas.addEventListener('wheel', (event) => {
     }, 100 /* ms */);
 });
 
-function updateLabels(event: MouseEvent) {
-    lCenterX.textContent = centerX.toString();
-    lCenterY.textContent = centerY.toString();
-    lEventX.textContent = event.offsetX.toString(); 
-    lEventY.textContent = event.offsetY.toString();
-    lX0.textContent = ((0 - canvasWidth / 2) / (scale * canvasWidth) + centerX).toString();
-    lY0.textContent = ((0 - canvasHeight / 2) / (scale * canvasHeight) + centerY).toString();
-    lX1.textContent = ((canvasWidth-1 - canvasWidth / 2) / (scale * canvasWidth) + centerX).toString();
-    lY1.textContent = ((canvasHeight-1 - canvasHeight / 2) / (scale * canvasHeight) + centerY).toString();
-}
+/*
+ * Panning with the mouse
+ */
 
 canvas.addEventListener('mousedown', (event) => {
     isPanning = true;
     startX = event.offsetX;
     startY = event.offsetY;
-    oldCenterX = centerX;
-    oldCenterY = centerY;
+    oldCX = logical.cx;
+    oldCY = logical.cy;
     fastMode();
     drawMandelbrot();
     updateLabels(event);
@@ -88,8 +114,9 @@ canvas.addEventListener('mousedown', (event) => {
 
 canvas.addEventListener('mousemove', (event) => {
     if (!isPanning) return;
-    centerX = oldCenterX - (event.offsetX - startX) / (scale * canvasWidth);
-    centerY = oldCenterY - (event.offsetY - startY) / (scale * canvasHeight);
+    // changed canvas to client values
+    logical.cx = oldCX - (event.offsetX - startX) / scale;
+    logical.cy = oldCY - (event.offsetY - startY) / scale;
     updateLabels(event);
     updateURL();
     drawMandelbrot();
@@ -101,56 +128,17 @@ canvas.addEventListener('mouseup', () => {
     drawMandelbrot();
 });
 
-function fastMode() {
-    canvas.width = 200;
-    canvas.height = 200;
-    canvasWidth = canvas.width;
-    canvasHeight = canvas.height;
-}
-
-function preciseMode() {
-    canvas.width = 800;
-    canvas.height = 800;
-    canvasWidth = canvas.width;
-    canvasHeight = canvas.height;
-}
-
-function drawMandelbrot() {
-    if (!ctx) return;
-    const iterMax = 360;
-    const hcw = canvasWidth / 2, hch = canvasHeight / 2;
-    const scw = scale * canvasWidth, sch = scale * canvasHeight;
-    for (let cx = 0; cx < canvasWidth; cx++) {
-	for (let cy = 0; cy < canvasHeight; cy++) {
-	    let iter = 0;
-	    let x0 = (cx - hcw) / scw + centerX;
-	    let y0 = (cy - hch) / sch + centerY;
-	    let x = 0, y = 0, x2 = 0, y2 = 0;
-	    while (x2 + y2 < 4 && iter < iterMax) {
-		y = (x + x) * y + y0;
-		x = x2 - y2 + x0;
-		x2 = x * x;
-		y2 = y * y;
-		iter++;
-	    }
-	    if (iter == iterMax) {
-		ctx.fillStyle = '#000';
-	    } else {
-		const hue = (250 - iter) % iterMax;
-		ctx.fillStyle = `hsl(${hue}, 100%, 50%)`;
-	    }
-	    ctx.fillRect(cx, cy, 1, 1);
-	}
-    }
-}
+/*
+ * Panning with touch actions
+ */
 
 canvas.addEventListener('touchstart', (event) => {
     const touch = event.touches[0];
     isPanning = true;
     startX = touch.pageX;
     startY = touch.pageY;
-    oldCenterX = centerX;
-    oldCenterY = centerY;
+    oldCX = logical.cx;
+    oldCY = logical.cy;
     fastMode();
     event.preventDefault(); // Prevent scrolling and zooming by the browser
 }, { passive: false });
@@ -158,8 +146,8 @@ canvas.addEventListener('touchstart', (event) => {
 canvas.addEventListener('touchmove', (event) => {
     if (!isPanning) return;
     const touch = event.touches[0];
-    centerX = oldCenterX - (touch.pageX - startX) / (scale * canvasWidth);
-    centerY = oldCenterY - (touch.pageY - startY) / (scale * canvasHeight);
+    logical.cx = oldCX - (touch.pageX - startX) / scale;
+    logical.cy = oldCY - (touch.pageY - startY) / scale;
     updateURL();
     drawMandelbrot();
     event.preventDefault(); // Prevent scrolling and zooming by the browser
@@ -207,3 +195,51 @@ canvas.addEventListener('touchend', (event) => {
 	drawMandelbrot();
     }
 });
+
+/*
+ * Rendering logic
+ */
+
+function fastMode() {
+    canvas.width = canvas.clientWidth / 10;
+    canvas.height = canvas.clientHeight / 10;
+}
+
+function preciseMode() {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+}
+
+function drawMandelbrot() {
+    if (!ctx) return;
+    const iterMax = 360;
+    const canvasWidth = canvas.width, canvasHeight = canvas.height;
+    const hcw = canvas.width / 2, hch = canvas.height / 2;
+    const scw = scale * canvas.width, sch = scale * canvas.height;
+    let [lx, ly] = logical.upperLeft();
+    let [ldx, ldy] = [logical.width / canvasWidth, logical.height / canvasHeight];
+    let x0 = lx;
+    for (let cx = 0; cx < canvasWidth; cx++) {
+	let y0 = ly;
+	for (let cy = 0; cy < canvasHeight; cy++) {
+	    let iter = 0;
+	    let x = 0, y = 0, x2 = 0, y2 = 0;
+	    while (x2 + y2 < 4 && iter < iterMax) {
+		y = (x + x) * y + y0;
+		x = x2 - y2 + x0;
+		x2 = x * x;
+		y2 = y * y;
+		iter++;
+	    }
+	    if (iter == iterMax) {
+		ctx.fillStyle = '#000';
+	    } else {
+		const hue = (250 - iter) % iterMax;
+		ctx.fillStyle = `hsl(${hue}, 100%, 50%)`;
+	    }
+	    ctx.fillRect(cx, cy, 1, 1);
+	    y0 += ldy;
+	}
+	x0 += ldx;
+    }
+}
