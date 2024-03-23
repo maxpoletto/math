a97714a713f3f75bf54a0d5c7979ad4462800082
Max Poletto <maxp@google.com>
2024-03-23 09:43:06 +0100

    dead code

--- main.ts
+++ main.ts
@@ -283,39 +283,3 @@ function drawMandelbrot() {
 	});
     }
 }
-
-/*
-function drawMandelbrot() {
-    if (!ctx) return;
-    updateMetadata();
-    console.log(`draw ${canvas.width}`);
-    const iterMax = 360;
-    const canvasWidth = canvas.width, canvasHeight = canvas.height;
-    const [ldx, ldy] = [logical.width / canvasWidth, logical.height / canvasHeight];
-    let [lx, ly] = logical.upperLeft();
-    let x0 = lx;
-    for (let cx = 0; cx < canvasWidth; cx++) {
-	let y0 = ly;
-	for (let cy = 0; cy < canvasHeight; cy++) {
-	    let iter = 0;
-	    let x = 0, y = 0, x2 = 0, y2 = 0;
-	    while (x2 + y2 < 4 && iter < iterMax) {
-		y = (x + x) * y + y0;
-		x = x2 - y2 + x0;
-		x2 = x * x;
-		y2 = y * y;
-		iter++;
-	    }
-	    if (iter == iterMax) {
-		ctx.fillStyle = '#000';
-	    } else {
-		const hue = (250 - iter) % iterMax;
-		ctx.fillStyle = `hsl(${hue}, 100%, 50%)`;
-	    }
-	    ctx.fillRect(cx, cy, 1, 1);
-	    y0 += ldy;
-	}
-	x0 += ldx;
-    }
-}
-*/
